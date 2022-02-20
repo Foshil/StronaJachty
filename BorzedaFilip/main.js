41a9b356c7b5e682f9646824fac486e2b8dbe60f
Foshil <38264814+Foshil@users.noreply.github.com>
2022-02-20 22:13:12 +0100

    Delete BorzedaFilip directory

--- BorzedaFilip/main.js
+++
@@ -1,7 +0,0 @@
-function openF(id) {
-    document.getElementById(`check_display${id}`).classList.remove('check_display')
-}
-
-function closeF(id) {
-    document.getElementById(`check_display${id}`).classList.add('check_display')
-}
