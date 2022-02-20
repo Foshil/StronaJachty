84bc71e52e438f31f563d6e010d49aac8873cc0e
Foshil <38264814+Foshil@users.noreply.github.com>
2022-02-20 22:13:45 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function openF(id) {
+    document.getElementById(`check_display${id}`).classList.remove('check_display')
+}
+
+function closeF(id) {
+    document.getElementById(`check_display${id}`).classList.add('check_display')
+}
